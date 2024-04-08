12336c426ccec4fb367cb0b92428f068ce1a0f47
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-08 11:37:46 -0700

    add logic to update state for R moves

--- src/get-state.py
+++ src/get-state.py
@@ -4,4 +4,16 @@ def rotate_face_clockwise(face):
     """
     return [face[6], face[3], face[0], face[7], face[4], face[1], face[8], face[5], face[2]]
 
+def update_state_for_r_move(state):
+    """
+    Updates the cube state for an R move.
+    """
+    new_state = {face: stickers[:] for face, stickers in state.items()}  # Deep copy of the state
+    new_state['R'] = rotate_face_clockwise(state['R'])
+    new_state['U'][2], new_state['U'][5], new_state['U'][8] = state['F'][2], state['F'][5], state['F'][8]
+    new_state['F'][2], new_state['F'][5], new_state['F'][8] = state['D'][2], state['D'][5], state['D'][8]
+    new_state['D'][2], new_state['D'][5], new_state['D'][8] = state['B'][6], state['B'][3], state['B'][0]
+    new_state['B'][0], new_state['B'][3], new_state['B'][6] = state['U'][2], state['U'][5], state['U'][8]
+    return new_state
+
 
