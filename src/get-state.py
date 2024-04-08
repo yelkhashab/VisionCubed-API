e3d3c48adb48b06c531044018212a313e9c4e472
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-08 12:59:02 -0700

    add L, B, D move logic

--- src/get-state.py
+++ src/get-state.py
@@ -16,6 +16,18 @@ def update_state_for_r_move(state):
     new_state['B'][6], new_state['B'][3], new_state['B'][0] = state['U'][2], state['U'][5], state['U'][8]
     return new_state
 
+def update_state_for_l_move(state):
+    """
+    Updates the cube state for an L move.
+    """
+    new_state = {face: stickers[:] for face, stickers in state.items()}  # Deep copy of the state
+    new_state['L'] = rotate_face_clockwise(state['L'])
+    new_state['U'][0], new_state['U'][3], new_state['U'][6] = state['B'][8], state['B'][5], state['B'][2]
+    new_state['F'][0], new_state['F'][3], new_state['F'][6] = state['U'][0], state['U'][3], state['U'][6]
+    new_state['D'][0], new_state['D'][3], new_state['D'][6] = state['F'][0], state['F'][3], state['F'][6]
+    new_state['B'][2], new_state['B'][5], new_state['B'][8] = state['D'][6], state['D'][3], state['D'][0]
+    return new_state
+
 def update_state_for_u_move(state):
     """
     Correctly updates the cube state for a U move.
@@ -28,6 +40,18 @@ def update_state_for_u_move(state):
     new_state['L'][0], new_state['L'][1], new_state['L'][2] = state['F'][0], state['F'][1], state['F'][2]
     return new_state
 
+def update_state_for_d_move(state):
+    """
+    Updates the cube state for a D move.
+    """
+    new_state = {face: stickers[:] for face, stickers in state.items()}
+    new_state['D'] = rotate_face_clockwise(state['D'])
+    new_state['F'][6], new_state['F'][7], new_state['F'][8] = state['L'][6], state['L'][7], state['L'][8]
+    new_state['R'][6], new_state['R'][7], new_state['R'][8] = state['F'][6], state['F'][7], state['F'][8]
+    new_state['B'][6], new_state['B'][7], new_state['B'][8] = state['R'][6], state['R'][7], state['R'][8]
+    new_state['L'][6], new_state['L'][7], new_state['L'][8] = state['B'][6], state['B'][7], state['B'][8]
+    return new_state
+
 def update_state_for_f_move(state):
     """
     Updates the cube state for an F move.
@@ -38,6 +62,18 @@ def update_state_for_f_move(state):
     new_state['L'][2], new_state['L'][5], new_state['L'][8] = state['D'][0], state['D'][1], state['D'][2]
     new_state['D'][0], new_state['D'][1], new_state['D'][2] = state['R'][6], state['R'][3], state['R'][0]
     new_state['R'][0], new_state['R'][3], new_state['R'][6] = state['U'][6], state['U'][7], state['U'][8]
+    return new_state
+
+def update_state_for_b_move(state):
+    """
+    Updates the cube state for a B move.
+    """
+    new_state = {face: stickers[:] for face, stickers in state.items()}
+    new_state['B'] = rotate_face_clockwise(state['B'])
+    new_state['U'][0], new_state['U'][1], new_state['U'][2] = state['R'][2], state['R'][5], state['R'][8]
+    new_state['L'][0], new_state['L'][3], new_state['L'][6] = state['U'][0], state['U'][1], state['U'][2]
+    new_state['D'][6], new_state['D'][7], new_state['D'][8] = state['L'][0], state['L'][3], state['L'][6]
+    new_state['R'][2], new_state['R'][5], new_state['R'][8] = state['D'][8], state['D'][7], state['D'][6]
     return new_state
 
 # Updating the RubiksCube class to include corrected move implementations
@@ -55,11 +91,16 @@ class RubiksCubeCorrected:
     def apply_move(self, move):
         if move == 'R':
             self.state = update_state_for_r_move(self.state)
+        elif move == 'L':
+            self.state = update_state_for_l_move(self.state)
         elif move == 'U':
             self.state = update_state_for_u_move(self.state)
+        elif move == 'D':
+            self.state = update_state_for_d_move(self.state)
         elif move == 'F':
             self.state = update_state_for_f_move(self.state)
-        # Additional moves can be added following the pattern
+        elif move == 'B':
+            self.state = update_state_for_b_move(self.state)
     
     def apply_moves(self, moves):
         for move in moves:
@@ -70,7 +111,7 @@ class RubiksCubeCorrected:
 
 # Testing the corrected implementation
 cube = RubiksCubeCorrected()
-cube.apply_moves(['R', 'U', 'F', 'R', 'U', 'U', 'R', 'F', 'F', 'R', 'R', 'F'])
+cube.apply_moves(['F', 'R', 'U', 'L', 'B', 'D'])
 state = cube.get_state()
 print(state)
 
