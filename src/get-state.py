16a168a2b7bea855a7bf73b4377cc20e0a62aa03
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-08 11:38:53 -0700

    add logic to update state for F moves

--- src/get-state.py
+++ src/get-state.py
@@ -25,4 +25,16 @@ def update_state_for_u_move(state):
     new_state['F'][:3], new_state['R'][:3], new_state['B'][:3], new_state['L'][:3] = state['R'][:3], state['B'][:3], state['L'][:3], state['F'][:3]
     return new_state
 
+def update_state_for_f_move(state):
+    """
+    Updates the cube state for an F move.
+    """
+    new_state = {face: stickers[:] for face, stickers in state.items()}  # Deep copy of the state
+    new_state['F'] = rotate_face_clockwise(state['F'])
+    new_state['U'][6], new_state['U'][7], new_state['U'][8] = state['L'][8], state['L'][5], state['L'][2]
+    new_state['L'][2], new_state['L'][5], new_state['L'][8] = state['D'][0], state['D'][1], state['D'][2]
+    new_state['D'][0], new_state['D'][1], new_state['D'][2] = state['R'][6], state['R'][3], state['R'][0]
+    new_state['R'][0], new_state['R'][3], new_state['R'][6] = state['U'][6], state['U'][7], state['U'][8]
+    return new_state
+
 
