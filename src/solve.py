012a6cab40c9ada7c01a175ddf9c14ba37e43d6c
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-09 10:28:36 -0700

    add comment to demonstrate how the net is generated

--- src/solve.py
+++ src/solve.py
@@ -1,6 +1,37 @@
 import kociemba
 
 def convertToNet(state):
+    """
+    This function takes a dictionary of faces where each face is represented
+    by an array of nine cubies and their colors. The cubies are ordered as shown
+    in the net below. It replaces the colors with the letters U, R, F, D, L, and B, 
+    which stand for Up, Right, Front, Down, Left, and Back, respectively to allow 
+    for solving from any starting orientation.
+
+                     |------------|
+                     | U1  U2  U3 |
+                     |            |
+                     | U4  U5  U6 |
+                     |            |
+                     | U7  U8  U9 |
+        |------------|------------|------------|------------|
+        | L1  L2  L3 | F1  F2  F3 | R1  R2  R3 | B1  B2  B3 |
+        |            |            |            |            |
+        | L4  L5  L6 | F4  F5  F6 | R4  R5  R6 | B4  B5  B6 |
+        |            |            |            |            |
+        | L7  L8  L9 | F7  F8  F9 | R7  R8  R9 | B7  B8  B9 |
+        |------------|------------|------------|------------|
+                     | D1  D2  D3 |
+                     |            |
+                     | D4  D5  D6 |
+                     |            |
+                     | D7  D8  D9 |
+                     |------------|
+
+    The function returns a string that represents the state of the cube in the 
+    format that Kociemba's algorithm expects. The pieces are ordered as follows:
+    U1U2U3...R1R2R3...F1F2F3...D1D2D3...L1L2L3...B1B2B3...
+    """
     order = ['U', 'R', 'F', 'D', 'L', 'B']
     net = ''
     conversion_dict = {'W': 'U', 'B': 'R', 'R': 'F', 'Y': 'D', 'G': 'L', 'O': 'B'}
