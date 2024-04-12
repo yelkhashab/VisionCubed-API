becba61c146016ad1c4a28979709a5f1b3e954e7
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-11 17:18:22 -0700

    Refactor scan_image function to accept multiple base64-encoded images in JSON format

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # Assuming these modules are correctly set up
-from src.face_scan import scan
+from src.relative_face_scan import scan
 from src.get_state import RubiksCube
 from src.scrambler import generate_scramble
 from src.solve import solve
@@ -12,8 +12,12 @@ CORS(app)  # Enable CORS for all domains
 
 @app.route('/api/scan', methods=['POST'])
 def scan_image():
-    image = request.get_json().get('image')
-    result = scan(image)  # Call the scan function with the image
+    images = request.get_json()
+    
+    # Extract the base64-encoded images from the request
+    raw_images = [images['F'], images['R'], images['B'], images['L'], images['U'], images['D']]
+
+    result = scan(raw_images)  # Call the scan function with the raw_images
     if 'unknown' in result:
         return jsonify({'error': 'Error scanning'}), 400  # Return 400 error code if result contains 'unknown'
     return jsonify(result)  # Return the result as a JSON response
@@ -33,10 +37,6 @@ def api_solve():
     state = request.get_json()  # Get the state dictionary from the request
     solution = solve(state)  # Call the solve function with the state dictionary
     return jsonify({'solution': solution})  # Return the result as a JSON response
-
-# @app.before_first_request
-# def before_first_request():
-#     print("The server is up and ready to handle requests!")
 
 if __name__ == '__main__':
     app.run(debug=True)  
