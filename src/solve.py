1ae75aa8a7aed2cf7c8872c7ce8cb7c56d6f9401
Youssef El Khashab <youssef_khashab@hotmail.com>
2024-04-12 21:42:20 -0700

    Refactor solve function to return a dictionary with net and solution in solve.py

--- src/solve.py
+++ src/solve.py
@@ -49,7 +49,11 @@ def convertToNet(state):
 
 def solve(state):
     net = convertToNet(state)
-    return kociemba.solve(net)
+    res = {
+        'net': net,
+        'solution': kociemba.solve(net)
+    }
+    return res
 
 # state =  {'F': ['R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9'], 'B': ['O1', 'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9'], 'U': ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9'], 'D': ['Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8', 'Y9'], 'L': ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9'], 'R': ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9']}
 # state = {'F': ['R' for i in range(9)], 'B': ['O' for i in range(9)], 'U': ['W' for i in range(9)], 'D': ['Y' for i in range(9)], 'L': ['G' for i in range(9)], 'R': ['B' for i in range(9)]}
